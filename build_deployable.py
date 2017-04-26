25dd971c218fc315f7ede1d05d4947b3664a11d2
Stefan Ravn van Overeem <stefan@stefanrvo.dk>
2017-04-26 22:42:07 +0200

    Test wine cx_freeze

--- build_deployable.py
+++ build_deployable.py
@@ -3,10 +3,11 @@ import sys
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {}
+build_exe_options = { 'include_msvcr': True,}
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
+bdist_msi_options = { }
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
@@ -14,5 +15,5 @@ if sys.platform == "win32":
 setup(  name = "PyJeopardy",
         version = "0.1",
         description = "PyJeopardy!",
-        options = {"build_exe": build_exe_options},
+        options = {"build_exe": build_exe_options, "bdist_msi": bdist_msi_options},
         executables = [Executable("main.py", base=base)])
